5af8000cd63aeb42e39a264a3e7b241c09e7792c
3k-kleva <koffi.kougblenou@etud.u-picardie.fr>
2019-06-16 21:38:53 +0000

    new uptade

--- src/main/java/ResoudreGrille.java
+++ src/main/java/ResoudreGrille.java
@@ -1,11 +1,14 @@
 /**
 * Permet de resoudre une grille de sudoku .
 */
-public class ResoudreGrille {
-  /** Pour cacher le contructeur */
-  private ResoudreGrille() {}
-  /** Point d'entrée du programe. 
-   *  @param args
+public final class ResoudreGrille {
+  /**
+   * Pour cacher le contructeur.
+   */
+  private ResoudreGrille() { }
+  /**
+   * Point d'entrée du programe.
+   * @param args liste d'argument
    */
   public static void main(final String[] args) {
     //GrilleImpl lagrille = new GrilleImpl();
